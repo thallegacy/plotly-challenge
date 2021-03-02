e7c98bcb49f84314334e7eb4376f8b9a7016fe5b
thallegacy <thallegacy@users.noreply.github.com>
2021-03-01 21:04:26 -0500

    simple gauge

--- static/js/app.js
+++ static/js/app.js
@@ -177,7 +177,7 @@ function init() {
         {
           domain: { x: [0, 1], y: [0, 1] },
           value: filteredData.wfreq,
-          title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
+          automargin: true,
           type: "indicator",
           mode: "gauge+number"
         }
